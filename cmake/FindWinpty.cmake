513ceae7228ea9583452349c7cdc884ebe72a0a2
wxt1221 <3264117476@qq.com>
2024-01-30 12:29:04 +0800

    fix build

--- cmake/FindWinpty.cmake
+++ cmake/FindWinpty.cmake
@@ -1,5 +1,17 @@
 include(LibFindMacros)
 
+GetBinaryDep(TARGET winpty
+    INSTALL_COMMAND ${CMAKE_COMMAND} -E make_directory ${DEPS_INSTALL_DIR}/bin
+      COMMAND ${CMAKE_COMMAND} -DFROM_GLOB=${DEPS_BUILD_DIR}/src/winpty/${TARGET_ARCH}/bin/*
+        -DTO=${DEPS_INSTALL_DIR}/bin/
+        -P ${CMAKE_CURRENT_SOURCE_DIR}/cmake/CopyFilesGlob.cmake
+      COMMAND ${CMAKE_COMMAND} -DFROM_GLOB=${DEPS_BUILD_DIR}/src/winpty/include/*
+        -DTO=${DEPS_INSTALL_DIR}/include/
+        -P ${CMAKE_CURRENT_SOURCE_DIR}/cmake/CopyFilesGlob.cmake
+      COMMAND ${CMAKE_COMMAND} -DFROM_GLOB=${DEPS_BUILD_DIR}/src/winpty/${TARGET_ARCH}/lib/*
+        -DTO=${DEPS_INSTALL_DIR}/lib/
+        -P ${CMAKE_CURRENT_SOURCE_DIR}/cmake/CopyFilesGlob.cmake)
+
 find_path(WINPTY_INCLUDE_DIR winpty.h)
 set(WINPTY_INCLUDE_DIRS ${WINPTY_INCLUDE_DIR})
 
@@ -8,3 +20,4 @@ find_program(WINPTY_AGENT_EXE winpty-agent.exe)
 set(WINPTY_LIBRARIES ${WINPTY_LIBRARY})
 
 find_package_handle_standard_args(Winpty DEFAULT_MSG WINPTY_LIBRARY WINPTY_INCLUDE_DIR)
+
